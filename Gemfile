007b44df5e07173df1d64c887727d353d190cc3d
Nec <p.tatarkin@gmail.com>
2013-06-13 19:24:06 +0400

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -1,15 +1,19 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
-
+gem 'pg', '0.12.2'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 group :development, :test do
-gem 'sqlite3', '1.3.5'
+#gem 'sqlite3', '1.3.5'
 gem 'rspec-rails', '2.11.0'
+gem 'guard-rspec', '1.2.1'
+gem 'guard-spork', '1.2.0'
+gem 'spork', '0.9.2'
+gem 'childprocess', '0.3.6'
 end
 # Gems used only for assets and not required
-# in production environments by default.
+# in production endvironments by default.
 group :assets do
   gem 'sass-rails',   '3.2.5'
   gem 'coffee-rails', '3.2.2'
@@ -24,12 +28,13 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
 gem 'capybara', '1.1.2'
-gem 'addressable', '2.3.4'
+gem 'rb-inotify', '~> 0.9'
+gem 'libnotify', '0.5.9'
 end
 
-group :production do
-gem 'pg', '0.12.2'
-end
+#group :production do
+#gem 'pg', '0.12.2'
+#end
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
-require 'addressable/uri'
+#require 'addressable/uri'
 
-@uri = Addressable::URI.parse("/static_page/home")
+
 
 describe "Static pages" do
+
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have content 'Sample App'" do
       visit 'http://localhost:3000/static_pages/home'
       page.should have_selector('h1', :text=>'Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit 'http://localhost:3000/static_pages/home'
-      page.should have_selector('title', :text=>'Ruby on Rails Tutorial Sample App | Home')
+      page.should have_selector('title', :text=>"#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit 'http://localhost:3000/static_pages/home'
+      page.should_not have_selector('title', :text=>'| Home')
     end
   end
 
@@ -24,7 +32,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit 'http://localhost:3000/static_pages/help'
-      page.should have_selector('title', :text=>'| Help')
+      page.should have_selector('title', :text=>"#{base_title} | Help")
     end
   end
 
@@ -36,8 +44,19 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit 'http://localhost:3000/static_pages/about'
-      page.should have_selector('title', :text=>'| About')
+      page.should have_selector('title', :text=>"#{base_title} | About")
     end
   end
 
+  describe "Contact page" do
+    it "Contact test" do
+      visit 'http://localhost:3000/static_pages/contact'
+      page.should have_selector('h1', :text=>'Contact')
+    end
+
+    it "should have the righn title" do
+      visit 'http://localhost:3000/static_pages/contact'
+      page.should have_selector('title', :text=>"#{base_title} | Contact")
+    end
+  end
 end
